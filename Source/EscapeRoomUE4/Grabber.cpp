53d0d5607fba3a28d5a963b4d9889c7a23cc9005
jgm88 <jgm88@outlook.es>
2016-12-22 18:44:41 +0000

    Testing raycasting

---
+++ Source/EscapeRoomUE4/Grabber.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EscapeRoomUE4.h"
+#include "Grabber.h"
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	UE_LOG(LogTemp, Warning, TEXT("GRABBER HERE"));
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// Get the player view
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		PlayerViewPointLocation, 
+		PlayerViewPointRotation
+	);
+
+	UE_LOG(
+		LogTemp, Warning, TEXT("GRABBER Location %s Rotation %s"),
+		*PlayerViewPointLocation.ToString(),
+		*PlayerViewPointRotation.ToString()
+	);
+
+	FVector LineTraceEnd = PlayerViewPointLocation + TraceDistance;
+	DrawDebugLine(
+		GetWorld(), 
+		PlayerViewPointLocation, 
+		LineTraceEnd, 
+		FColor(255, 0, 0),
+		false, 0.f, 0.f, 10.f
+	);
+
+
+	
+}
+
