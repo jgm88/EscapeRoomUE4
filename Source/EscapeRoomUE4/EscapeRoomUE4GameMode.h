01375ad83e9ef6b653fdbe348828525dda229a34
jgm88 <jgm88@outlook.es>
2016-12-22 16:41:07 +0000

    Lost Triger Volume * Added new default pawn

---
+++ Source/EscapeRoomUE4/EscapeRoomUE4GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "EscapeRoomUE4GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOMUE4_API AEscapeRoomUE4GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
