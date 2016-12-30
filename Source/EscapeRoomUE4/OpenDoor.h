2e625ce94b17e0a240dbf1d75abd62a07b741c4e
jgm88 <jgm88@outlook.es>
2016-12-30 22:09:07 +0000

    Added test sound to the door

--- Source/EscapeRoomUE4/OpenDoor.h
+++ Source/EscapeRoomUE4/OpenDoor.h
@@ -5,6 +5,9 @@
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
+// Made a Class
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorEvent);
+
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ESCAPEROOMUE4_API UOpenDoor : public UActorComponent
@@ -18,21 +21,19 @@ public:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-	void OpenDoor();
-	void CloseDoor();
-
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
+	UPROPERTY(BlueprintAssignable)
+	FDoorEvent OnOpen;
+
+	UPROPERTY(BlueprintAssignable)
+	FDoorEvent OnClose;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	//FRotator InitialRotation;
+
 private:
-
-	float LastTimeOpenDoor = 0.f;
-
-	UPROPERTY(EditAnywhere)
-	float DoorCloseDelay = 1.f;
-
-	UPROPERTY(EditAnywhere)
-	float OpenAngle = -90.f;
 
 	UPROPERTY(EditAnywhere)
 	ATriggerVolume* PressurePlate = nullptr;
